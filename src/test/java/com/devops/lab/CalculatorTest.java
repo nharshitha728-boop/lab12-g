effec98bd2979907bbb29e753f2e7891c8e4fed3
Harshitha <nharshitha728@gmail.com>
2026-04-08 22:04:28 +0530

    fix: clear merge markers from CalculatorTest

--- src/test/java/com/devops/lab/CalculatorTest.java
+++ src/test/java/com/devops/lab/CalculatorTest.java
@@ -1,21 +1,14 @@
 package com.devops.lab;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
-
     Calculator calculator = new Calculator();
 
     @Test
-<<<<<<< HEAD
     void testSubtract() {
-
-        assertEquals(5, calculator.subtract(10,5));
-
-=======
-    void testMultiply() {
-        assertEquals(20, calculator.multiply(4,5));
->>>>>>> 223c26b7f42cf0b7cab9f4462ae39e1b3cbcb1e2
+        // For Part B (Gradle), we are testing subtraction
+        assertEquals(5, calculator.subtract(10, 5));
     }
 }
